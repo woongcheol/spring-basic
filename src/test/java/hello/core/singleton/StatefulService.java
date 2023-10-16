3a488ab89fd3be249c482b220eaee1ca1ced2dce
woongcheol Shin <5starshin@gmail.com>
2023-10-16 19:15:38 +0900

    feat: Spring 컨테이너 기반 싱글톤 객체 여부 테스트 코드 구현

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,15 +2,17 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price; // 상태를 유지하는 필드
+    // private int price; // 상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 문제 지점
-    }
+        // this.price = price; // 문제 지점
 
-    public int getPrice() {
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -15,17 +15,17 @@ class SatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // Thread A : A 사용자 10,000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         // Thread B : B 사용자 20,000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         // Thread A : A 사용자 주문 금핵 조회
-        int price = statefulService1.getPrice();
+        // int price = statefulService1.getPrice();
 
-        System.out.println("price = " + price);
+        System.out.println("price = " + userAPrice);
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
 
     }
 
