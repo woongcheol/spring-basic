84d01d01092263667c417954eb859ce83f3d40ff
woongcheol Shin <5starshin@gmail.com>
2023-10-24 22:46:36 +0900

    feat: Qualifier -> Primay 수정

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000; // 고정 할인 금액
