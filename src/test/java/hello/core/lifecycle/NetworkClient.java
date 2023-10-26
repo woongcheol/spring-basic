a2427218eec053da80a1efc49be9618515800add
woongcheol Shin <5starshin@gmail.com>
2023-10-26 11:30:40 +0900

    refactor: 빈 생명주기 콜백 annotation 방식 변경

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient{
 
     private String url;
@@ -27,11 +30,14 @@ public class NetworkClient{
         
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지 ");
     }
+
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disConnect();
